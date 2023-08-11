3f07cc58748879de9b692a34b35a1f5269a7325d
Amirhossein Azizi <amirhosseinazizi10@gmail.com>
2023-08-11 20:31:39 +0330

    random mac address and encryption

--- main.py
+++ main.py
@@ -4,19 +4,48 @@ import psutil
 import secrets
 import string
 import re
+import faker
 
 conf.verb = 0 # Disable Scapy logging
+last_message_pattern = ''
+fake = faker.Faker()
+
+def encrypt(plain_text, key):
+    encrypted_text = ""
+    key_index = 0
+    
+    for char in plain_text:
+        key_char = key[key_index % len(key)]
+        encrypted_char = chr((ord(char) + ord(key_char)) % 256)
+        encrypted_text += encrypted_char
+        key_index += 1
+    
+    return encrypted_text
+
+def decrypt(encrypted_text, key):
+    decrypted_text = ""
+    key_index = 0
+    
+    for char in encrypted_text:
+        key_char = key[key_index % len(key)]
+        decrypted_char = chr((ord(char) - ord(key_char)) % 256)
+        decrypted_text += decrypted_char
+        key_index += 1
+    
+    return decrypted_text
+
 
 def generate_unique_pattern(length=16):
     characters = string.ascii_letters + string.digits + string.punctuation
     unique_pattern = ''.join(secrets.choice(characters) for _ in range(length))
     return unique_pattern
 
-def list_ifaces():
+def list_ifaces(): # List of network interfaces along with their names and MAC addresses.
     addrs = psutil.net_if_addrs()
     ifaces = []
     mac_addresses = []
-    for i, key in enumerate(addrs.keys()):
+    i = 1
+    for key in addrs.keys(): # Avoid using 'enumerate' since it exhibits different behaviors on various operating systems.
         addresses = addrs[key]
         for addr in addresses:
             if addr.family == psutil.AF_LINK:  # Check for MAC address
@@ -24,12 +53,16 @@ def list_ifaces():
                 print(f"{i}- ({key}): {mac_address}")
                 ifaces.append(key)
                 mac_addresses.append(mac_address)
+                i += 1
                 break
     return ifaces, mac_addresses
 
 regex_pattern = r'^(.*?):(.*?):(.*)$'
 
-def packet_handler(packet, title, unique_pattern):
+# Packets consist of two layers. The first layer is Ethernet, which includes a destination address of ff:ff:ff:ff:ff:ff (Broadcast) and a source address that can either be the user's MAC address or a randomly generated MAC address.
+# The second layer is RAW, comprising three parts: the chatroom name (not encrypted), the encrypted message (encrypted with the password), and a unique pattern designed to prevent clients from receiving their own messages. (A new unique pattern is generated for each message.)
+
+def packet_handler(packet, title, key): 
     if packet.haslayer(Raw):
         payload = packet[Raw].load
         title_bytes = f"{title}:".encode('utf-8')
@@ -37,26 +70,36 @@ def packet_handler(packet, title, unique_pattern):
             payload = payload.decode('utf-8')
             matches = re.match(regex_pattern, payload)
             if matches:
-                header = matches.group(1)
                 message = matches.group(2)
-                unique_ptrn = matches.group(3)
-                if unique_pattern == unique_ptrn:
+                pattern = matches.group(3)
+                if last_message_pattern == pattern:
                     return
-                print(message)
+                print(decrypt(message, key))
 
-def start_sniffing(title, interface, unique_pattern):
-    sniff(iface=interface, prn=lambda pkt: packet_handler(pkt, title, unique_pattern), store=0)
+def start_sniffing(title, interface, key):
+    sniff(iface=interface, prn=lambda pkt: packet_handler(pkt, title, key), store=0)
 
-def send_packet(header, data, interface, mac_address, unique_pattern):
+def send_packet(header, data, interface, mac_address, key, name, anonymous):
+    global last_message_pattern
+    last_message_pattern = generate_unique_pattern(16) # This is used to prevent clients from receiving their own messages
     ether_source = mac_address
-    eth = Ether(src = ether_source, dst = 'ff:ff:ff:ff:ff:ff')
-    r = Raw(load=f"{header}:{data}:{unique_pattern}")
-    x = sendp(eth/r, iface=interface)
+    message = name + ": " + data
+    if anonymous:
+        ether_source = fake.hexify('^^:^^:^^:^^:^^:^^')
+        message = "Anonymous: " + data
+    ethernet = Ether(src = ether_source, dst = 'ff:ff:ff:ff:ff:ff')
+    raw = Raw(load=f"{header}:{encrypt(message, key)}:{last_message_pattern}")
+    sendp(ethernet/raw, iface=interface)
 
 def get_interface():
     ifaces, mac_addresses = list_ifaces()
+    regex_pattern = r'\d+$'
     while True:
-        index = int(input("Please select the interface number you wish to chat with: "))
+        index = input("Please select the interface number you wish to chat with: ")
+        if not re.match(regex_pattern, index):
+            print("Invalid command!")
+            continue
+        index = int(index)
         if index > 0 and index <= len(ifaces):
             index -= 1
             break
@@ -77,27 +120,31 @@ def get_header_and_password():
 
 def get_name():
     name = input("Please provide your name (press enter to remain anonymous): ")
-    return name, len(name) == 0 # To check if user want to remain anonymous
+    return name, len(name) == 0 # To check if the user wants to remain anonymous or not
 
-def receive_messages(header, iface, unique_pattern):
-    sniff_thread = threading.Thread(target=start_sniffing, args=(header,iface,unique_pattern,), daemon = True)
+def receive_messages(header, iface, password):
+    sniff_thread = threading.Thread(target=start_sniffing, args=(header, iface, password,), daemon = True)
     sniff_thread.start()
 
-def send_messages(iface, mac_address, header, unique_pattern, name, anonymous):
+def send_messages(iface, mac_address, header, name, anonymous, password):
+    print("Send '\\' to exit")
+    single_back_slash = r'^\\$'
+    multiple_back_slash = r'^\\{2,}$'
     while(True):
         message = input()
-        send_packet(header, message, iface, mac_address, unique_pattern)
-
-packet_title = 'ECV1' # For first version of Easy Communicate
-packet_data = 'This is the content of the broadcast packet.'
+        if re.match(single_back_slash, message):
+            break
+        elif re.match(multiple_back_slash, message):
+            message = message[1:]
+        send_packet(header, message, iface, mac_address, password, name, anonymous)
 
 if __name__ == "__main__":
     print("Welcome to Easy Communicate V1.0.0")
-    unique_pattern = generate_unique_pattern(16)
     iface, mac_address = get_interface()
     header, password = get_header_and_password()
     name, anonymous = get_name()
-    receive_messages(header, iface, unique_pattern) # It just starts a daemon thread to receive messages
-    send_messages(iface, mac_address, header, unique_pattern, name, anonymous)
+    receive_messages(header, iface, password) # This code begins a daemon thread that waits for messages.
+    send_messages(iface, mac_address, header, name, anonymous, password)
+    print("Goodbye :)")
 
 
